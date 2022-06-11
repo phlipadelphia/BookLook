1c5fc368e40969980cee2ea35d3f641ce130a5f7
Zephyr <phillip.t.mock@gmail.com>
2022-06-10 21:34:09 -0700

    last touches before submit

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { ApolloServer, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const server = new ApolloServer({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
-      <>
+      <ApolloProvider server={server}>
         <Navbar />
         <Routes>
           <Route 
@@ -23,7 +30,7 @@ function App() {
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
         </Routes>
-      </>
+      </ApolloProvider>
     </Router>
   );
 }
